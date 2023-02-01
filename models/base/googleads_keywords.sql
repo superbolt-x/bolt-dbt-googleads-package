7957ffea4c8a7707e8338c94eba0050f4c7ac68f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-02-01 11:24:16 +0100

    Update googleads_keywords.sql

--- models/base/googleads_keywords.sql
+++ models/base/googleads_keywords.sql
@@ -1,24 +1,27 @@
 {%- set selected_fields = [
     "ad_group_id",
-    "ad_group_criterion_criterion_id",
+    "id",
     "keyword_match_type",
     "keyword_text",
-    "date"
+    "negative",
+    "status",
+    "cpc_bid_micros",
+    "updated_at"
 ] -%}
 {%- set schema_name, table_name = 'googleads_raw', 'keywords' -%}
 
 WITH staging AS 
     (SELECT 
-        {% for field in selected_fields|reject("eq","date") -%}
+        {% for field in selected_fields|reject("eq","updated_at") -%}
         {{ get_googleads_clean_field(table_name, field) }}
         {%- if not loop.last %},{%- endif %}
         {% endfor -%}
     FROM 
         (SELECT
             {{ selected_fields|join(", ") }},
-            MAX(date) OVER (PARTITION BY ad_group_id, ad_group_criterion_criterion_id) as last_updated_at
+            MAX(updated_at) OVER (PARTITION BY ad_group_id, id) as last_updated_at
         FROM {{ source(schema_name, table_name) }})
-    WHERE date = last_updated_at
+    WHERE updated_at = last_updated_at
     )
 
 SELECT *,
