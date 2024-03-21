3d82582edd4c78a2d594e3e8b749b14aa3fd0424
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-21 11:45:44 -0400

    Create googleads_asset_groups.sql

---
+++ models/base/googleads_asset_groups.sql
@@ -0,0 +1,27 @@
+{%- set selected_fields = [
+    "customer_id",
+    "id",
+    "name",
+    "status",
+    "advertising_channel_type",
+    "updated_at"
+] -%}
+{%- set schema_name, table_name = 'gsheet_raw', 'asset_groups' -%}
+
+WITH staging AS 
+    (SELECT 
+        {% for field in selected_fields|reject("eq","updated_at") -%}
+        {{ get_googleads_clean_field(table_name, field) }}
+        {%- if not loop.last %},{%- endif %}
+        {% endfor -%}
+    FROM 
+        (SELECT
+            {{ selected_fields|join(", ") }},
+            MAX(updated_at) OVER (PARTITION BY id) as last_updated_at
+        FROM {{ source(schema_name, table_name) }})
+    WHERE updated_at = last_updated_at
+    )
+
+SELECT *,
+    asset_group_id as unique_key
+FROM staging 
