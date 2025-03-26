9735f19450455c9f2fac7195d0681d667f4d0033
Vincent Gefflaut <vincent.gefflaut@superbolt.agency>
2025-03-26 17:43:40 -0400

    rollback

--- models/reporting/googleads_performance_by_campaign_consolidated.sql
+++ models/reporting/googleads_performance_by_campaign_consolidated.sql
@@ -122,13 +122,14 @@ WITH
     ),
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
-{%- set exclude_fields_for_measures = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
+{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
 {%- set dimensions = ['campaign_id'] -%}
-{%- set measures = stg_fields
-                    |reject("in",exclude_fields_for_measures)
+{%- set measures = adapter.get_columns_in_relation(ref('googleads_campaigns_insights'))
+                    |map(attribute="name")
+                    |reject("in",exclude_fields)
                     |reject("in",dimensions)
                     |list
-                    -%}
+                    -%}  
  
     {%- for date_granularity in date_granularity_list %}
 
