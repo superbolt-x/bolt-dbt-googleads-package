888d55e55ab62d9ebd2d1a3741017733aea1fbb1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-18 12:16:18 +0200

    Update googleads_campaign_products_insights.sql

--- models/base/googleads_campaign_products_insights.sql
+++ models/base/googleads_campaign_products_insights.sql
@@ -5,10 +5,13 @@
 
 {%- set exclude_fields = [
     "unique_key",
+    "_fivetran_synced",
     "account_id",
+    "account_name",
     "account_currency_code",
     "campaign_name",
-    "account_name",
+    "campaign_start_date",
+    "campaign_end_date",
     "campaign_status",
     "advertising_channel_type",
     "gmail_saves",
@@ -28,6 +31,7 @@
     "active_view_measurability",
     "active_view_viewability",
     "active_view_measurable_cost_micros",
+    "last_updated",
     "_fivetran_synced"
 ]
 -%}
